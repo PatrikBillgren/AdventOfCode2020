68dade748bb76ba025aa856f3eaf5b73769cd688
Patrik Billgren <patrik.billgren@telia.no>
2020-12-02 11:52:28 +0100

    Small versions day 2

--- 2/2/program-small.py
+++ 2/2/program-small.py
@@ -1,6 +1,7 @@
 import re
-def check(line):
-    m = re.search('^(\d+)-(\d+) ([a-z]): ([a-z]+)$', line) 
-    return (m.group(4)[int(m.group(1)) - 1] == m.group(3)) != (m.group(4)[int(m.group(2)) - 1] == m.group(3))
-print(sum(map(check, [line.rstrip('\n') for line in open('input')])))
+p = '^(\d+)-(\d+) ([a-z]): ([a-z]+)$'
+def condition(m):
+    passw = m.group(4)
+    return (passw[int(m.group(1)) - 1] == m.group(3)) != (passw[int(m.group(2)) - 1] == m.group(3))
 
+print(sum(map(condition, [re.search(p, l) for l in open('i.txt')])))
