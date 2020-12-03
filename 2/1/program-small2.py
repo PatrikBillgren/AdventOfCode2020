f6fb8b200e3235fc8c3e5b4dc7c42f354da77852
Patrik Billgren <patrik.billgren@telia.no>
2020-12-03 23:51:58 +0100

    Refactoring

--- 2/1/program-small2.py
+++ 2/1/program-small2.py
@@ -1,3 +1,3 @@
 import re
-p = '^(\d+)-(\d+) ([a-z]).*'
-print(sum(map(lambda m: int(m.group(1)) <= m.group(0).count(m.group(3)) - 1 <= int(m.group(2)), [re.search(p, l) for l in open('i.txt')])))
+p = '^(\d+)-(\d+) (.).*'
+print(sum(map(lambda m: int(m[1]) <= m[0].count(m[3]) - 1 <= int(m[2]), [re.match(p, l) for l in open('i.txt')])))
--- 2/2/program-small.py
+++ 2/2/program-small.py
@@ -3,5 +3,4 @@ p = '^(\d+)-(\d+) ([a-z]): ([a-z]+)$'
 def condition(m):
     passw = m.group(4)
     return (passw[int(m.group(1)) - 1] == m.group(3)) != (passw[int(m.group(2)) - 1] == m.group(3))
-
 print(sum(map(condition, [re.search(p, l) for l in open('i.txt')])))
