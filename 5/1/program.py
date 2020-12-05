e3e2932294728832c12c311506a116b026de2279
Patrik Billgren <patrik.billgren@telia.no>
2020-12-05 15:45:28 +0100

    Simplified

--- 5/1/program.py
+++ 5/1/program.py
@@ -1,2 +1,1 @@
-lines = [line.rstrip('\n') for line in open('input')]
-print(max(int(l.replace('B', '1').replace('F', '0').replace('R', '1').replace('L', '0'), 2) for l in lines))
+print(max(int(l.replace('B', '1').replace('F', '0').replace('R', '1').replace('L', '0'), 2) for l in open('input')))
--- 5/2/program.py
+++ 5/2/program.py
@@ -1,4 +1,3 @@
-lines = [line.rstrip('\n') for line in open('input')]
-found = set(int(l.replace('B', '1').replace('F', '0').replace('R', '1').replace('L', '0'), 2) for l in lines)
+found = set(int(l.replace('B', '1').replace('F', '0').replace('R', '1').replace('L', '0'), 2) for l in open('input'))
 allSeats = set(range(894))
 print(allSeats - found)
